158054ef5d1bed10d73dbc08acbefdb4b3cacdc2
pedrohhcunha <phhcpedrohhcunha@gmail.com>
2021-06-20 18:23:40 -0300

    Babel with JSX Alteration

--- dist.js
+++ dist.js
@@ -8,35 +8,35 @@ function MyComponent1() {
     value: myName
   }, /*#__PURE__*/React.createElement("div", {
     className: "component1"
-  }, /*#__PURE__*/React.createElement(MyComponent2, null)));
+  }, /*#__PURE__*/React.createElement(MyComponent2, null))); // Return a context and a DIV element than contains MyComponent
 }
 
 function MyComponent2() {
   //Create component 2
   return /*#__PURE__*/React.createElement("div", {
     className: "component-2"
-  }, /*#__PURE__*/React.createElement(MyComponent3, null));
+  }, /*#__PURE__*/React.createElement(MyComponent3, null)); // Return a DIV element than contains MyComponent 3
 }
 
 function MyComponent3() {
   //Create component 3
   return /*#__PURE__*/React.createElement("div", {
     className: "component-3"
-  }, /*#__PURE__*/React.createElement(MyComponent4, null));
+  }, /*#__PURE__*/React.createElement(MyComponent4, null)); // Return a DIV element than contains MyComponent 4
 }
 
 function MyComponent4() {
   //Create component 4
   return /*#__PURE__*/React.createElement(NameContext.Consumer, null, NameContext => /*#__PURE__*/React.createElement("div", {
     className: "component-4"
-  }, /*#__PURE__*/React.createElement("p", null, "My name is ", NameContext, " and I am 17 years Old!")));
+  }, /*#__PURE__*/React.createElement("p", null, "My name is ", NameContext, " and I am 17 years Old!"))); //Return a DIV element than contains p element with the text and variable contatenation as a content
 }
 
 function Components() {
   //Create a main components
   return /*#__PURE__*/React.createElement("div", {
     id: "components"
-  }, /*#__PURE__*/React.createElement(MyComponent1, null));
+  }, /*#__PURE__*/React.createElement(MyComponent1, null)); // Return a DIV element than contains MyComponent
 }
 
 ReactDOM.render(
